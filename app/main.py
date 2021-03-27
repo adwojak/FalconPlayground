f71765ff567eb2d8d0cf8ccfd81d1ddf9e400436
Artur <dwojaka207@gmail.com>
2021-03-27 17:20:14 +0100

    Changed from Fastapi to Falcon

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from falcon import API
+
+
+class QuoteResource:
+
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'Grace Hopper'
+        }
+
+        resp.media = quote
+
+
+api = API()
+api.add_route('/quote', QuoteResource())
