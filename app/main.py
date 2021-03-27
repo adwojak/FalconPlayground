fef3262f41aaa0734bc83d93a38f68f6ced41c1e
Artur <dwojaka207@gmail.com>
2021-03-27 17:23:05 +0100

    Refactored

--- app/main.py
+++ app/main.py
@@ -2,19 +2,17 @@ from falcon import API
 
 
 class QuoteResource:
-
     def on_get(self, req, resp):
         """Handles GET requests"""
         quote = {
-            'quote': (
-                "I've always been more interested in "
-                "the future than in the past."
+            "quote": (
+                "I've always been more interested in " "the future than in the past."
             ),
-            'author': 'Grace Hopper'
+            "author": "Grace Hopper",
         }
 
         resp.media = quote
 
 
 api = API()
-api.add_route('/quote', QuoteResource())
+api.add_route("/quote", QuoteResource())
