a5c06c03998a4cb0b9fe9f166f911fae94c5bc67
Artur <dwojaka207@gmail.com>
2021-03-28 16:18:07 +0200

    Added xml response

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from simplexml import dumps as simplexml_dumps
+from flask import Flask, make_response
 from flask_restful import Api, Resource
 
 
@@ -7,12 +8,21 @@ class SomeResource(Resource):
         # from flask import request
         # request.content_type
         # request.data
-        return jsonify({"hello": "world"})
+        return {"hello": "world"}
+
+
+def output_xml(data, code, headers=None):
+    # Representations
+    """Makes a Flask response with a XML encoded body"""
+    resp = make_response(simplexml_dumps({"response": data}), code)
+    resp.headers.extend(headers or {})
+    return resp
 
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app)
+    api = Api(app, default_mediatype="application/xml")
+    api.representations = {"application/xml": output_xml}
 
     api.add_resource(SomeResource, "/")
 
